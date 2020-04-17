7f1c3e15be5e3dd0d50c75b3aad85fb41978d65e
Maci <maci1011@facebook.com>
2020-04-18 00:08:43 +0200

    general edits

--- Controller/MediaController.php
+++ Controller/MediaController.php
@@ -9,116 +9,116 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class MediaController extends AbstractController
 {
-    public function indexAction()
-    {
-    	$list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Media')
-            ->getList();
+	public function indexAction()
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Media')
+			->getList();
 
-        return $this->render('MaciPageBundle:Media:index.html.twig', array('list' => $list));
-    }
+		return $this->render('MaciPageBundle:Media:index.html.twig', array('list' => $list));
+	}
 
-    public function tagAction($id)
-    {
-        $tag = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Tag')
-            ->findOneById($id);
+	public function tagAction($id)
+	{
+		$tag = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Tag')
+			->findOneById($id);
 
-        if (!$tag) {
-            return $this->redirect($this->generateUrl('maci_media'));
-        }
+		if (!$tag) {
+			return $this->redirect($this->generateUrl('maci_media'));
+		}
 
-        return $this->render('MaciPageBundle:Media:tag.html.twig', array('tag' => $tag));
-    }
+		return $this->render('MaciPageBundle:Media:tag.html.twig', array('tag' => $tag));
+	}
 
-    public function tagsAction($type)
-    {
-        $list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Tag')
-            ->findBy(array(
-                'type' => $type
-            ));
+	public function tagsAction($type)
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Tag')
+			->findBy(array(
+				'type' => $type
+			));
 
-        return $this->render('MaciPageBundle:Media:tags.html.twig', array('list' => $list));
-    }
+		return $this->render('MaciPageBundle:Media:tags.html.twig', array('list' => $list));
+	}
 
-    public function brandsAction()
-    {
-        $list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Tag')
-            ->findBy(array(
-                'type' => 'brand'
-            ));
+	public function brandsAction()
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Tag')
+			->findBy(array(
+				'type' => 'brand'
+			));
 
-        return $this->render('MaciPageBundle:Media:brands.html.twig', array('list' => $list));
-    }
+		return $this->render('MaciPageBundle:Media:brands.html.twig', array('list' => $list));
+	}
 
-    public function galleryAction($type)
-    {
-        $list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Album')
-            ->getGallery($type);
+	public function galleryAction($type)
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Album')
+			->getGallery($type);
 
-        return $this->render('MaciPageBundle:Media:gallery.html.twig', array('list' => $list));
-    }
+		return $this->render('MaciPageBundle:Media:gallery.html.twig', array('list' => $list));
+	}
 
-    public function albumAction($id)
-    {
-        $album = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Album')
-            ->findOneById($id);
+	public function albumAction($id)
+	{
+		$album = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Album')
+			->findOneById($id);
 
-        if (!$album) {
-            return $this->redirect($this->generateUrl('maci_media'));
-        }
+		if (!$album) {
+			return $this->redirect($this->generateUrl('maci_media'));
+		}
 
-        return $this->render('MaciPageBundle:Media:album.html.twig', array('album' => $album));
-    }
+		return $this->render('MaciPageBundle:Media:album.html.twig', array('album' => $album));
+	}
 
-    public function sliderAction($id)
-    {
-        $list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Album')
-            ->getOneById($id);
+	public function sliderAction($id)
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Album')
+			->getOneById($id);
 
-        return $this->render('MaciPageBundle:Media:_slider.html.twig', array('album' => $album));
-    }
+		return $this->render('MaciPageBundle:Media:_slider.html.twig', array('album' => $album));
+	}
 
-    public function libraryAction()
-    {
-        $list = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Permission')
-            ->findBy(array(
-                'user' => $this->getUser(),
-                'status' => 'end'
-            ));
+	public function libraryAction()
+	{
+		$list = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Permission')
+			->findBy(array(
+				'user' => $this->getUser(),
+				'status' => 'end'
+			));
 
-        return $this->render('MaciPageBundle:Media:library.html.twig', array('list' => $list));
-    }
+		return $this->render('MaciPageBundle:Media:library.html.twig', array('list' => $list));
+	}
 
-    public function getAction($path)
-    {
-        $media = $this->getDoctrine()->getManager()
-            ->getRepository('MaciPageBundle:Media\Media')
-            ->findOneByPath($path);
+	public function getAction($path)
+	{
+		$media = $this->getDoctrine()->getManager()
+			->getRepository('MaciPageBundle:Media\Media')
+			->findOneByPath($path);
 
-        if ($media) {
-            if (!$media->getPublic()) {
-                $permission = $this->getDoctrine()->getManager()
-                    ->getRepository('MaciPageBundle:Media\Permission')
-                    ->findOneBy(array(
-                        'user' => $this->getUser(),
-                        'media' => $media,
-                        'status' => 'end'
-                    ));
-                if ($permission) {
-                    return new BinaryFileResponse($media->getAbsolutePath());
-                }
-                return new Response();
-            } else {
-                return new BinaryFileResponse($media->getAbsolutePath());
-            }
-        }
-        return new Response();
-    }
+		if ($media) {
+			if (!$media->getPublic()) {
+				$permission = $this->getDoctrine()->getManager()
+					->getRepository('MaciPageBundle:Media\Permission')
+					->findOneBy(array(
+						'user' => $this->getUser(),
+						'media' => $media,
+						'status' => 'end'
+					));
+				if ($permission) {
+					return new BinaryFileResponse($media->getAbsolutePath());
+				}
+				return new Response();
+			} else {
+				return new BinaryFileResponse($media->getAbsolutePath());
+			}
+		}
+		return new Response();
+	}
 }
