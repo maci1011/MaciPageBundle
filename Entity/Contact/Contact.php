e4df2d20fb3e341a70cee4d9bcae79a3da26e2c8
Maci <maci1011@facebook.com>
2021-08-08 23:50:30 +0200

    general edits

--- Entity/Contact/Contact.php
+++ Entity/Contact/Contact.php
@@ -6,178 +6,209 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class Contact
 {
-    /**
-     * @var bigint $id
-     */
-    private $id;
+	/**
+	 * @var bigint $id
+	 */
+	private $id;
 
-    /**
-     * @var string $name
-     */
-    private $name;
+	/**
+	 * @var string $name
+	 */
+	private $name;
 
-    /**
-     * @var string $surname
-     */
-    private $surname;
+	/**
+	 * @var string $surname
+	 */
+	private $surname;
 
-    /**
-     * @var string $email
-     */
-    private $email;
+	/**
+	 * @var string $email
+	 */
+	private $email;
 
-    /**
-     * @var text $message
-     */
-    private $message;
+	/**
+	 * @var text $message
+	 */
+	private $message;
 
-    /**
-     * @var string $privacy
-     */
-    protected $privacy;
+	/**
+	 * @var \Doctrine\Common\Collections\Collection
+	 */
+	private $media;
 
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    private $media;
+	/**
+	 * @var \DateTime
+	 */
+	private $created;
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->media = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->media = new \Doctrine\Common\Collections\ArrayCollection();
+	}
 
 
-    /**
-     * Get id
-     *
-     * @return bigint 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return bigint 
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+	/**
+	 * Set name
+	 *
+	 * @param string $name
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
 
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+	/**
+	 * Get name
+	 *
+	 * @return string 
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
 
-    /**
-     * Set surname
-     *
-     * @param string $surname
-     */
-    public function setSurname($surname)
-    {
-        $this->surname = $surname;
-    }
+	/**
+	 * Set surname
+	 *
+	 * @param string $surname
+	 */
+	public function setSurname($surname)
+	{
+		$this->surname = $surname;
+	}
 
-    /**
-     * Get surname
-     *
-     * @return string 
-     */
-    public function getSurname()
-    {
-        return $this->surname;
-    }
+	/**
+	 * Get surname
+	 *
+	 * @return string 
+	 */
+	public function getSurname()
+	{
+		return $this->surname;
+	}
 
-    /**
-     * Set email
-     *
-     * @param string $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
+	/**
+	 * Set email
+	 *
+	 * @param string $email
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
 
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
+	/**
+	 * Get email
+	 *
+	 * @return string 
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
 
-    /**
-     * Set message
-     *
-     * @param text $message
-     */
-    public function setMessage($message)
-    {
-        $this->message = $message;
-    }
+	/**
+	 * Set message
+	 *
+	 * @param text $message
+	 */
+	public function setMessage($message)
+	{
+		$this->message = $message;
+	}
 
-    /**
-     * Get message
-     *
-     * @return text 
-     */
-    public function getMessage()
-    {
-        return $this->message;
-    }
+	/**
+	 * Get message
+	 *
+	 * @return text 
+	 */
+	public function getMessage()
+	{
+		return $this->message;
+	}
 
-    /**
-     * Add media
-     *
-     * @param \Maci\PageBundle\Entity\Media $media
-     * @return Contact
-     */
-    public function addMedia(\Maci\PageBundle\Entity\Media\Media $media)
-    {
-        $this->media[] = $media;
+	/**
+	 * Set created
+	 *
+	 * @param \DateTime $created
+	 * @return Post
+	 */
+	public function setCreated($created)
+	{
+		$this->created = $created;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Remove media
-     *
-     * @param \Maci\PageBundle\Entity\Media $media
-     */
-    public function removeMedia(\Maci\PageBundle\Entity\Media\Media $media)
-    {
-        $this->media->removeElement($media);
-    }
+	/**
+	 * Get created
+	 *
+	 * @return \DateTime 
+	 */
+	public function getCreated()
+	{
+		return $this->created;
+	}
 
-    /**
-     * Get media
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getMedia()
-    {
-        return $this->media;
-    }
+	/**
+	 * setCreatedValue
+	 */
+	public function setCreatedValue()
+	{
+		$this->created = new \DateTime();
+	}
 
-    /**
-     * __toString()
-     */
-    public function __toString()
-    {
-        return ( $this->getName().' '.$this->getSurname() );
-    }
+	/**
+	 * Add media
+	 *
+	 * @param \Maci\PageBundle\Entity\Media $media
+	 * @return Contact
+	 */
+	public function addMedia(\Maci\PageBundle\Entity\Media\Media $media)
+	{
+		$this->media[] = $media;
+
+		return $this;
+	}
+
+	/**
+	 * Remove media
+	 *
+	 * @param \Maci\PageBundle\Entity\Media $media
+	 */
+	public function removeMedia(\Maci\PageBundle\Entity\Media\Media $media)
+	{
+		$this->media->removeElement($media);
+	}
+
+	/**
+	 * Get media
+	 *
+	 * @return \Doctrine\Common\Collections\Collection 
+	 */
+	public function getMedia()
+	{
+		return $this->media;
+	}
+
+	/**
+	 * __toString()
+	 */
+	public function __toString()
+	{
+		return ( $this->getName().' '.$this->getSurname() );
+	}
 }
