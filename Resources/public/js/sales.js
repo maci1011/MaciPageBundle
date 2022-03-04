9df45f87b6381d988e1ab10e55bf79aef27a0b69
Maci <maci1011@facebook.com>
2022-03-04 03:25:32 +0100

    general edits

--- Resources/public/js/sales.js
+++ Resources/public/js/sales.js
@@ -34,7 +34,7 @@ var maciShopImport = function (options) {
 	},
 
 	setParent: function(id) {
-		var select = form.find('#import_set');
+		var select = form.find('#sales_set');
 		if (select.val() == "null") return;
 		$.ajax({
 			type: 'POST',
@@ -97,23 +97,26 @@ var maciShopImport = function (options) {
 			{
 				$("<div/>").addClass('col-xs-6').appendTo(row).html('<strong>' + vals.join(', ') + ':</strong>');
 				for (var j = 0; j < item.data.variants.length; j++) {
+					var btnClass = 'btn-info';
+					if (parseInt(data.list[i]['data']['variants'][j]['quantity']) < 1) btnClass = 'btn-danger';;
 					row = $("<div/>").addClass('row').css('padding', '2px').appendTo(wrapper);
 					$("<div/>").addClass('col-xs-6 text-right').css('paddingTop', '6px').html('<strong>' + data.list[i]['data']['variants'][j]['name'] + '</strong>').appendTo(row);
-					var variantItem = item.data.variants[j]
-					$("<button/>").addClass('btn btn-info').appendTo(row).click(function (e) {
+					$("<button/>").text('Select').addClass('btn ' + btnClass).appendTo(row).click(function (e) {
 						e.preventDefault();
-						_obj.saveRecord({id: item.id, variant: variantItem});
+						_obj.saveRecord($(this).prop('saveRecordData'));
 						barcodeInput.val('');
 						wrapper.remove();
 						wrapper = false;
 						barcodeInput.focus();
-					}).text('Select').wrap("<div/>").parent().addClass('col-xs-6');
+					}).prop('saveRecordData', {id: item.id, variant: item.data.variants[j]}).wrap("<div/>").parent().addClass('col-xs-6');
 				}
 			}
 			else
 			{
+				var btnClass = 'btn-info';
+				if (parseInt(data.list[i]['quantity']) < 1) btnClass = 'btn-danger';
 				$("<div/>").addClass('col-xs-6 text-right').css('paddingTop', '6px').appendTo(row).html('<strong>' + vals.join(', ') + '</strong>');
-				$("<button/>").addClass('btn btn-info').appendTo(row).click(function (e) {
+				$("<button/>").addClass('btn ' + btnClass).appendTo(row).click(function (e) {
 					e.preventDefault();
 					_obj.saveRecord({id: item.id});
 					barcodeInput.val('');
