85af6240b62787c84a44701c5aa7f65e2dc70457
Maci <maci1011@facebook.com>
2022-09-06 00:56:33 +0200

    general edits

--- Form/Order/MailType.php
+++ Form/Order/MailType.php
@@ -5,12 +5,19 @@ namespace Maci\PageBundle\Form\Order;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Maci\TranslatorBundle\Controller\TranslatorController;
 
 class MailType extends AbstractType
 {
+	protected $translator;
+
+	public function __construct(TranslatorController $translator)
+	{
+		$this->translator = $translator;
+	}
+
 	public function setDefaultOptions(OptionsResolverInterface $resolver)
 	{
 		$resolver->setDefaults(array(
@@ -22,8 +29,11 @@ class MailType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('mail', EmailType::class)
+			->add('mail', EmailType::class, [
+				'label' => $this->translator->getLabel('mail', 'Mail')
+			])
 			->add('use_this_email', SubmitType::class, [
+				'label' => $this->translator->getLabel('use-this-mail', 'Use This Mail'),
 				'attr' => ['class' => 'btn btn-primary']
 			])
 		;
